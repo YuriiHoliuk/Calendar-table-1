25e1a4c7d036d0a212fb2590942bd32db056c9b8
VladimirTi <tytmes@mail.ru>
2019-05-17 11:55:03 +0300

    solution

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ function createCalendar(ID, year, month) {
   let html = '<tr><th>ПН</th><th>ВТ</th><th>СР</th><th>ЧТ</th><th>ПТ</th><th>СБ</th><th>ВС</th></tr>'
   let temp = '';
   let date = new Date(year, month - 1);
-  let firstDay = date.getDay();
 
+  let firstDay = date.getDay() === 0 ? 7 : date.getDay();
   for(let i = 1; i < firstDay; i++) {
     temp += `<td></td>`
   }
 
   while(date.getMonth() + 1 === month) {
-    for (let i = date.getDay(); i < 8 && date.getMonth() + 1 === month; i++) {
+    const firstDayToAdd = date.getDay() === 0 ? 7 : date.getDay()
+    for (let i = firstDayToAdd; i < 8 && date.getMonth() + 1 === month; i++) {
       temp += `<td>${date.getDate()}</td>`;
       date.setDate(date.getDate() + 1)
     }
-
-    while(date.getDay() != 1) {
+    //console.log(date.getDay())
+    while(date.getDay() !== 1) {
       temp += `<td></td>`
       date.setDate(date.getDate() + 1)
     }
@@ -28,4 +29,4 @@ function createCalendar(ID, year, month) {
   document.getElementById(ID).innerHTML = html
 }
 
-createCalendar('calendar', 1994, 12)
+createCalendar('calendar', 2018, 7)
